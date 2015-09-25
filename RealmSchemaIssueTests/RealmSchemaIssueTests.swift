7ed637c37d33da043d71d530ee3df0cf1f0724ae
Aleksandar Kex Trpeski <atrpeski.kex@gmail.com>
2015-09-25 16:36:26 +0200

    Initial Commit

---
+++ RealmSchemaIssueTests/RealmSchemaIssueTests.swift
@@ -0,0 +1,36 @@
+//
+//  RealmSchemaIssueTests.swift
+//  RealmSchemaIssueTests
+//
+//  Created by Aleksandar Trpeski on 9/25/15.
+//  Copyright © 2015 Pristap. All rights reserved.
+//
+
+import XCTest
+@testable import RealmSchemaIssue
+
+class RealmSchemaIssueTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
